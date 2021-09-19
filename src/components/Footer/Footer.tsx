d1dd346e329991c1e83648c5f46134c450e3aa35
Feintty <makhmutoff.ildar2011@yandex.ru>
2021-09-19 13:34:53 +0300

    V-11 Создана верстка страницы ошибки V-11 Добавлена кнопка кнопка генерации ошибки V-11 Создан предохранитель для отлова ошибок

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.scss";
 
 const Footer: React.FC = () => {
+  const [createError, setCreateError] = useState(false);
+
+  if (createError) {
+    setCreateError(false);
+    throw new Error();
+  }
   return (
     <footer className="footer">
       <div className="footer__links">
         <a className="footer__link link-default">Главная страница</a>
-        <a className="footer__link link-default">Ссылка</a>
+        <a
+          className="footer__link link-default"
+          onClick={() => setCreateError(true)}
+        >
+          Сгенерировать ошибку
+        </a>
       </div>
       <div className="footer__copyright">Copyright © 2020 Simbirsoft</div>
     </footer>
